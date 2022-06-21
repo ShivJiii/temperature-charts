71e6afb5114cc4304ed1f21a1cf9e230447966b4
ShivJiii <shiv@arthtechsupports.com>
2022-06-21 14:01:31 +0530

    latest commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TemperatureMeasure from "./components/temparature-measure";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <TemperatureMeasure />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/temparature-measure.js
@@ -0,0 +1,76 @@
+import React, { Fragment, useState } from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import DateRangePicker from "@wojtekmaj/react-daterange-picker";
+
+const TemperatureMeasure = () => {
+  const [value, onChange] = useState([new Date(), new Date()]);
+
+  const options = {
+    title: {
+      text: "Environmental Temperature",
+    },
+
+    yAxis: {
+      title: {
+        text: "Total Temparature",
+      },
+    },
+
+    xAxis: {
+      accessibility: {
+        rangeDescription: "Range: 2010 to 2017",
+      },
+    },
+
+    legend: {
+      layout: "vertical",
+      align: "right",
+      verticalAlign: "middle",
+    },
+
+    plotOptions: {
+      series: {
+        label: {
+          connectorAllowed: false,
+        },
+        pointStart: value[0].getFullYear(),
+      },
+    },
+
+    series: [
+      {
+        name: "Temperature",
+        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+      },
+    ],
+
+    responsive: {
+      rules: [
+        {
+          condition: {
+            maxWidth: 500,
+          },
+          chartOptions: {
+            legend: {
+              layout: "horizontal",
+              align: "center",
+              verticalAlign: "bottom",
+            },
+          },
+        },
+      ],
+    },
+  };
+
+  return (
+    <Fragment>
+      <div className="date-picker-wrapper">
+        <DateRangePicker onChange={onChange} value={value} />
+      </div>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </Fragment>
+  );
+};
+
+export default TemperatureMeasure;
