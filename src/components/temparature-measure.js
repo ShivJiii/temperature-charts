d7662c93bbf127b3f60f4445452fd8d12680e4bb
ShivJiii <shiv@arthtechsupports.com>
2022-06-21 15:46:37 +0530

    latest commit

--- src/components/temparature-measure.js
+++ src/components/temparature-measure.js
@@ -1,15 +1,31 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import DateRangePicker from "@wojtekmaj/react-daterange-picker";
 
 const TemperatureMeasure = () => {
+  const year = new Date().getFullYear();
+  const month = new Date().getMonth();
+  const day = new Date().getDate();
+  const [value, onChange] = useState([
+    new Date(),
+    new Date(year, month, day + 8),
+  ]);
+  const [temprature, setTemprature] = useState([]);
 
-  const [value, onChange] = useState([new Date(), new Date()]);
-
+  const diffTime = Math.abs(value[0] - value[1]);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  console.log({ diffDays });
   const onChangeDate = (val) => {
     onChange(val);
   };
+  useEffect(() => {
+    let temp = [];
+    for (let i = 0; i < diffDays; i++) {
+      temp.push(Math.floor(Math.random() * 55) + 1);
+    }
+    setTemprature(temp);
+  }, [diffDays]);
 
   const options = {
     title: {
@@ -18,13 +34,19 @@ const TemperatureMeasure = () => {
 
     yAxis: {
       title: {
-        text: "Total Temparature",
+        text: "Average Temparature",
+      },
+      labels: {
+        format: "{value}°C",
       },
     },
 
     xAxis: {
       accessibility: {
         rangeDescription: "Range: 2010 to 2017",
+      },
+      labels: {
+        format: `{value}/${value[0].getMonth() + 1}`,
       },
     },
 
@@ -36,17 +58,18 @@ const TemperatureMeasure = () => {
 
     plotOptions: {
       series: {
+        title: "adsf",
         label: {
-          connectorAllowed: false,
+          title: `${value[0].getDate()}/${value[0].getMonth()}`,
         },
-        pointStart: value[0].getFullYear(),
+        pointStart: value[0].getDate(),
       },
     },
 
     series: [
       {
         name: "Temperature",
-        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+        data: [...temprature],
       },
     ],
 
@@ -71,7 +94,12 @@ const TemperatureMeasure = () => {
   return (
     <Fragment>
       <div className="date-picker-wrapper">
-        <DateRangePicker onChange={onChangeDate} value={value} />
+        <DateRangePicker
+          closeCalendar={true}
+          clearIcon={null}
+          onChange={onChangeDate}
+          value={value}
+        />
       </div>
       <HighchartsReact highcharts={Highcharts} options={options} />
     </Fragment>
